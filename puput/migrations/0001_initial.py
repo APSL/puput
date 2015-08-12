dc1ccf40207a1976626f5a00781bb62648b447db
Marc <marctc@gmail.com>
2015-08-12 23:20:10 +0200

    Fixed migrations

--- puput/migrations/0001_initial.py
+++ puput/migrations/0001_initial.py
@@ -2,29 +2,35 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import modelcluster.fields
-import datetime
-import django.db.models.deletion
 import modelcluster.contrib.taggit
+import datetime
 import wagtail.wagtailcore.fields
+import modelcluster.fields
+import django.db.models.deletion
 import puput.routes
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailcore', '0001_squashed_0016_change_page_url_path_to_text_field'),
-        ('wagtailimages', '0006_add_verbose_names'),
+        ('wagtailcore', '0002_auto_20150730_1219'),
         ('taggit', '0001_initial'),
+        ('wagtailimages', '0006_add_verbose_names'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='BlogPage',
             fields=[
-                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, serialize=False, primary_key=True, to='wagtailcore.Page')),
-                ('description', models.CharField(help_text='The page description that will appear under the title.', max_length=255, blank=True, verbose_name='Description')),
-                ('header_image', models.ForeignKey(blank=True, on_delete=django.db.models.deletion.SET_NULL, to='wagtailimages.Image', null=True, related_name='+', verbose_name='Header image')),
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, to='wagtailcore.Page', serialize=False, primary_key=True)),
+                ('description', models.CharField(blank=True, max_length=255, help_text='The page description that will appear under the title.', verbose_name='Description')),
+                ('display_comments', models.BooleanField(verbose_name='Display comments', default=False)),
+                ('display_categories', models.BooleanField(verbose_name='Display categories', default=True)),
+                ('display_tags', models.BooleanField(verbose_name='Display tags', default=True)),
+                ('display_popular_entries', models.BooleanField(verbose_name='Display popular entries', default=True)),
+                ('display_last_entries', models.BooleanField(verbose_name='Display last entries', default=True)),
+                ('display_archive', models.BooleanField(verbose_name='Display archive', default=True)),
+                ('header_image', models.ForeignKey(blank=True, verbose_name='Header image', on_delete=django.db.models.deletion.SET_NULL, to='wagtailimages.Image', related_name='+', null=True)),
             ],
             options={
                 'verbose_name': 'Blog',
@@ -34,59 +40,80 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('name', models.CharField(max_length=80, unique=True, verbose_name='Category Name')),
-                ('slug', models.SlugField(max_length=80, unique=True)),
-                ('description', models.CharField(max_length=500, blank=True)),
-                ('parent', models.ForeignKey(blank=True, null=True, related_name='children', to='puput.Category')),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=80, verbose_name='Category Name')),
+                ('slug', models.SlugField(unique=True, max_length=80)),
+                ('description', models.CharField(blank=True, max_length=500)),
+                ('parent', models.ForeignKey(blank=True, to='puput.Category', related_name='children', null=True)),
             ],
             options={
                 'ordering': ['name'],
                 'verbose_name_plural': 'Categories',
                 'verbose_name': 'Category',
             },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='CategoryEntryPage',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('category', models.ForeignKey(to='puput.Category', related_name='+', verbose_name='Category')),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('category', models.ForeignKey(verbose_name='Category', to='puput.Category', related_name='+')),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='EntryPage',
             fields=[
-                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, serialize=False, primary_key=True, to='wagtailcore.Page')),
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, to='wagtailcore.Page', serialize=False, primary_key=True)),
                 ('body', wagtail.wagtailcore.fields.RichTextField(verbose_name='body')),
-                ('date', models.DateTimeField(default=datetime.datetime.today, verbose_name='Post date')),
-                ('excerpt', wagtail.wagtailcore.fields.RichTextField(help_text='Used to display on puput pages list. If this field is not filled, a truncate version of body text will be used.', blank=True, verbose_name='excerpt')),
+                ('date', models.DateTimeField(verbose_name='Post date', default=datetime.datetime.today)),
+                ('excerpt', wagtail.wagtailcore.fields.RichTextField(blank=True, verbose_name='excerpt', help_text='Used to display on puput pages list. If this field is not filled, a truncate version of body text will be used.')),
                 ('num_comments', models.IntegerField(default=0, editable=False)),
-                ('categories', models.ManyToManyField(blank=True, to='puput.Category', through='puput.CategoryEntryPage')),
-                ('header_image', models.ForeignKey(blank=True, on_delete=django.db.models.deletion.SET_NULL, to='wagtailimages.Image', null=True, related_name='+', verbose_name='Header image')),
+                ('categories', models.ManyToManyField(to='puput.Category', blank=True, through='puput.CategoryEntryPage')),
+                ('header_image', models.ForeignKey(blank=True, verbose_name='Header image', on_delete=django.db.models.deletion.SET_NULL, to='wagtailimages.Image', related_name='+', null=True)),
             ],
             options={
-                'verbose_name_plural': 'Entries',
                 'verbose_name': 'Entry',
+                'verbose_name_plural': 'Entries',
             },
             bases=('wagtailcore.page',),
         ),
         migrations.CreateModel(
             name='EntryPageRelated',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('entrypage_from', modelcluster.fields.ParentalKey(to='puput.EntryPage', related_name='related_entrypage_from', verbose_name='Entry')),
-                ('entrypage_to', modelcluster.fields.ParentalKey(to='puput.EntryPage', related_name='related_entrypage_to', verbose_name='Entry')),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('entrypage_from', modelcluster.fields.ParentalKey(verbose_name='Entry', to='puput.EntryPage', related_name='related_entrypage_from')),
+                ('entrypage_to', modelcluster.fields.ParentalKey(verbose_name='Entry', to='puput.EntryPage', related_name='related_entrypage_to')),
             ],
+            options={
+            },
+            bases=(models.Model,),
         ),
         migrations.CreateModel(
             name='TagEntryPage',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
-                ('content_object', modelcluster.fields.ParentalKey(to='puput.EntryPage', related_name='entry_tags')),
+                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('content_object', modelcluster.fields.ParentalKey(related_name='entry_tags', to='puput.EntryPage')),
+                ('tag', models.ForeignKey(related_name='puput_tagentrypage_items', to='taggit.Tag')),
             ],
             options={
                 'abstract': False,
             },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='entrypage',
+            name='tags',
+            field=modelcluster.contrib.taggit.ClusterTaggableManager(blank=True, verbose_name='Tags', help_text='A comma-separated list of tags.', to='taggit.Tag', through='puput.TagEntryPage'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='categoryentrypage',
+            name='page',
+            field=modelcluster.fields.ParentalKey(related_name='entry_categories', to='puput.EntryPage'),
+            preserve_default=True,
         ),
         migrations.CreateModel(
             name='Tag',
@@ -96,20 +123,5 @@ class Migration(migrations.Migration):
                 'proxy': True,
             },
             bases=('taggit.tag',),
-        ),
-        migrations.AddField(
-            model_name='tagentrypage',
-            name='tag',
-            field=models.ForeignKey(to='taggit.Tag', related_name='puput_tagentrypage_items'),
-        ),
-        migrations.AddField(
-            model_name='entrypage',
-            name='tags',
-            field=modelcluster.contrib.taggit.ClusterTaggableManager(help_text='A comma-separated list of tags.', blank=True, through='puput.TagEntryPage', to='taggit.Tag', verbose_name='Tags'),
-        ),
-        migrations.AddField(
-            model_name='categoryentrypage',
-            name='page',
-            field=modelcluster.fields.ParentalKey(to='puput.EntryPage', related_name='entry_categories'),
         ),
     ]
