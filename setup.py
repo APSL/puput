aea8f0129cf424ae8c25ea1101210083059b76ce
nicklo <nick@ingredients.com.au>
2016-02-18 18:45:29 +1000

    Updating setup.py to use django-el-pagination

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     install_requires=[
         'Django>=1.7.1,<1.9',
         'wagtail>=1.0,<2.0',
-        'django-endless-pagination==2.0',
+        'django-el-pagination==2.1.1',
         'tapioca-disqus==0.1.2',
     ],
     url='http://github.com/APSL/puput',
