456894ca59eac37bb2d5b96c35013f440a5de486
Pieter De Decker <pdedecker@gmail.com>
2016-05-22 11:29:04 +0200

    Don't install Django==1.10a
    This version isn't compatible with Wagtail yet.

--- setup.py
+++ setup.py
@@ -40,7 +40,9 @@ setup(
     description='A Django blog app implemented in Wagtail.',
     long_description=codecs.open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8').read(),
     install_requires=[
-        'Django>=1.7.1',
+        # By default, pick the latest stable version of Django that's officially supported by Wagtail.
+        'Django>=1.7.1,<1.10',
+
         'django-compressor>=1.6',
         'wagtail>=1.0,<2.0',
         'django-el-pagination==2.1.1',
