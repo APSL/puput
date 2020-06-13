1eedf5ef8574e5aa2940428e8631b856957d0d13
Marc Tuduri <marctc@gmail.com>
2020-06-13 10:31:29 +0200

    Version 1.1.1

--- puput/__init__.py
+++ puput/__init__.py
@@ -1,30 +1,28 @@
-__author__ = 'Marc Tudurí'
-__email__ = 'marctc@gmail.com'
-__version__ = '1.1.0'
+__author__ = "Marc Tudurí"
+__email__ = "marctc@gmail.com"
+__version__ = "1.1.1"
 
 PUPUT_APPS = (
     # Wagtail apps
-    'wagtail.core',
-    'wagtail.admin',
-    'wagtail.documents',
-    'wagtail.snippets',
-    'wagtail.users',
-    'wagtail.images',
-    'wagtail.embeds',
-    'wagtail.search',
-    'wagtail.sites',
-    'wagtail.contrib.redirects',
-    'wagtail.contrib.forms',
-    'wagtail.contrib.sitemaps',
-    'wagtail.contrib.routable_page',
-
+    "wagtail.core",
+    "wagtail.admin",
+    "wagtail.documents",
+    "wagtail.snippets",
+    "wagtail.users",
+    "wagtail.images",
+    "wagtail.embeds",
+    "wagtail.search",
+    "wagtail.sites",
+    "wagtail.contrib.redirects",
+    "wagtail.contrib.forms",
+    "wagtail.contrib.sitemaps",
+    "wagtail.contrib.routable_page",
     # Third-party apps
-    'taggit',
-    'modelcluster',
-    'django_social_share',
-
+    "taggit",
+    "modelcluster",
+    "django_social_share",
     # Puput apps
-    'puput',
+    "puput",
 )
 
-default_app_config = 'puput.apps.PuputAppConfig'
+default_app_config = "puput.apps.PuputAppConfig"
