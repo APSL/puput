be10621840211ca788db0f20e3ee6040fab1faa3
Carlos Salom Oliver <csalom@apsl.net>
2022-12-13 16:46:45 +0100

    chore: update tests

--- tests/functional/test_entry_list.py
+++ tests/functional/test_entry_list.py
@@ -32,21 +32,12 @@ class TestEntryList:
 
     def test_entry_page_archive_year(self, client, blog_page, entry_page):
         _, domain, blog_path = blog_page.get_url_parts()
-        rq = client.get(f"{domain}{blog_path}{entry_page.slug}/")
+        rq = client.get(f"{domain}{blog_path}{entry_page.date.year}/")
         assert rq.status_code == self.expected_status_code
         assert "Entries for date" in str(rq.content)
 
-#
-#     def test_entry_page_archive_year(self, browser, site_url):
-#         browser.visit(site_url + '/blog/2016/')
-#         entries = browser.find_by_css('.page-content')
-#         assert browser.status_code == self.expected_status_code
-#         assert browser.is_text_present('Entries for date')
-#         assert len(entries) > 0
-#
-#     def test_entry_page_archive_year_month(self, browser, site_url):
-#         browser.visit(site_url + '/blog/2016/08/')
-#         entries = browser.find_by_css('.page-content')
-#         assert browser.status_code == self.expected_status_code
-#         assert browser.is_text_present('Entries for date')
-#         assert len(entries) > 0
+    def test_entry_page_archive_year_month(self, client, blog_page, entry_page):
+        _, domain, blog_path = blog_page.get_url_parts()
+        rq = client.get(f"{domain}{blog_path}{entry_page.date.year}/{entry_page.date.month}/")
+        assert rq.status_code == self.expected_status_code
+        assert "Entries for date" in str(rq.content)
