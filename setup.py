97a52348ff6ba1a0844ca844c00cac5154a27cc9
Francisco Fernandez <ffernandez@apsl.net>
2022-12-21 15:40:58 +0100

    lint

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
-        'Framework :: Django :: 3.2'
+        'Framework :: Django :: 3.2',
         'Framework :: Django :: 4.0',
         'Framework :: Django :: 4.1',
         'Intended Audience :: Developers',
